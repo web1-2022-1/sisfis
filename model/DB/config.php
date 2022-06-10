bf8aeabf1d9408b94892f37610da0e8e52d265dc
icaro2222 <icarodias2222@gmail.com>
2022-06-10 11:14:45 -0300

    colocando configurações do banco em um unico local

--- model/DB/config.php
+++ model/DB/config.php
@@ -3,4 +3,10 @@
 define('USER','icaro');
 define('PASS','tatakae');
 define('BASE','IFhealth');
+
+define('HOST','localhost');
+define('USUARIO','icaro');
+define('SENHA','tatakae');
+define('DB','IFhealth');
+ global $pdo;
 ?>
