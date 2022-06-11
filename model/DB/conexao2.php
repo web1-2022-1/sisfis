e9339143df8a82f8f64740feab296123a633fe1d
icaro2222 <icarodias2222@gmail.com>
2022-06-10 22:31:50 -0300

    controlando banco

--- model/DB/conexao2.php
+++ model/DB/conexao2.php
@@ -2,7 +2,8 @@
 error_reporting(E_ALL);
 ini_set("display_erros",1);
 session_start();
-require_once("/home/icaro/Documentos/Estudos/Densenvolvimento/Projeto_Web_I_Oficial/IFHelth-Beta/model/DB/config.php");
+
+require_once("config.php");
 try {
     $pdo = new PDO('mysql:host=' . HOST . '; dbname=' . BASE, USER, PASS);
     $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
