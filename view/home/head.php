a0e6e5df77680476094fc61a4e295cbbe7ce6684
icaro2222 <icarodias2222@gmail.com>
2022-06-12 07:25:56 -0300

    destrichando o dashboard

---
+++ view/home/head.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>IFHealth</title>
+	<link rel="stylesheet" type="text/css" <?php echo $css; ?> >
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+	<header>
+		<div class="container">
+			<div class="cabecario">
+				<div class="logo-topo">
+
+				</div>
+				<!--logo-topo-->
+				<div class="cabecariopt2">
+					<h3>Instituto Federal de Educação, Ciência e Tecnologia Baiano</h1>
+						<h1>Campus Guanambi</h1>
+						<li>
+							<a href="../login/logout.php">Sair</a>
+						</li>
+						<div class="borda-cabe">
+						</div>
+						<!--borda-cabe-->
+
+				</div>
+				<!--cabecariopt2 -->
+			</div>
+			<!--cabecario-->
+		</div>
+		<!--contaiver-->
+	</header>
