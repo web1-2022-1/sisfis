3d64ec38380b2d732857c87ae7f6f7dd3e11d5a1
icaro2222 <icarodias2222@gmail.com>
2022-06-16 15:24:26 -0300

    .

--- model/DB/config.php
+++ model/DB/config.php
@@ -4,20 +4,18 @@
 
 // // ------------ Banco Crud --------------
 
-// define('USER','icaro');
-// define('PASS','tatakae');
-// define('BASE','IFhealth');
-
 // define('USER', 'root');
 // define('PASS', '');
 // define('BASE', 'ifhealth');
 
 
-// ------------ Banco Para Hospedar Ícaro Dias --------------
+// ------------ Banco Para Hospedar no Mysql --------------
 
 // define('USER','id17504595_if');
 // define('PASS','YhR-nOB8l&K+/M9Y');
 // define('BASE','id17504595_ifhealth');
+
+// ------------ Banco Para Hospedar no Pgsql --------------
 
 define('HOST','ec2-52-72-56-59.compute-1.amazonaws.com');
 define('USER','zhxqqnaojtwbpu');
