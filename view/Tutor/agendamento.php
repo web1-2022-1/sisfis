dbd63753c02669e66e1f715be78856e27c34d7d8
icaro2222 <icarodias2222@gmail.com>
2022-06-25 00:31:14 -0300

    .

--- view/Tutor/agendamento.php
+++ view/Tutor/agendamento.php
@@ -5,7 +5,7 @@ ini_set("display_errors", 1);
 require_once(__DIR__.'/../../app/controller/Agendamento.php');
 require_once(__DIR__.'/../../app/controller/Disponibilidade.php');
 require_once(__DIR__.'/../../app/controller/Usuario.php');
-require_once(__DIR__.'/../../app/controller/');
+require_once(__DIR__.'/../../app/controller/Bloqueio.php');
 
 ?>
 
