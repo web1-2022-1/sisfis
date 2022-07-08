6f2546e633a8e9181233d9dd970bcb3eb58c0a6c
icaro2222 <icarodias2222@gmail.com>
2022-07-08 19:23:01 -0300

    .

--- model/DB/conexao.php
+++ model/DB/conexao.php
@@ -16,7 +16,7 @@ class DB {
         if (!isset(self::$instance)) {
             try {
                 self::$instance = new PDO('pgsql:host=' . HOST . ';port=' . PORT . '; dbname=' . BASE, USER, PASS);
-                self::$instance->exec("SET SCHEMA 'ifhealth';");
+                // self::$instance->exec("SET SCHEMA 'ifhealth';");
                 self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 self::$instance->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
             } catch (PDOException $e) {
