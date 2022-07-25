623519eab050e5c1c48625ba2f39ad55a557a67f
icaro2222 <icarodias2222@gmail.com>
2022-07-25 09:35:34 -0300

    .

--- model/DB/variaveis.php
+++ model/DB/variaveis.php
@@ -32,19 +32,19 @@
 // define('BASE','sisfis');
 
 
-define('HOST','postgres');
-define('USER','sisfis');
-define('PASS','sisfis2022');
-define('PORT','5432');
-define('BASE','sisfis');
+// define('HOST','postgres');
+// define('USER','sisfis');
+// define('PASS','sisfis2022');
+// define('PORT','5432');
+// define('BASE','sisfis');
 
 // ------------ Banco Para Hospedar no Pgsql --------------
 
-// define('HOST','ec2-52-72-56-59.compute-1.amazonaws.com');
-// define('USER','zhxqqnaojtwbpu');
-// define('PASS','5147bd0fc1127ed10f697b76e341d823c15cc4814db88c2c3b6d601218f7f8e3');
-// define('PORT','5432');
-// define('BASE','d9p5jd8s400ip5');
+define('HOST','ec2-52-72-56-59.compute-1.amazonaws.com');
+define('USER','zhxqqnaojtwbpu');
+define('PASS','5147bd0fc1127ed10f697b76e341d823c15cc4814db88c2c3b6d601218f7f8e3');
+define('PORT','5432');
+define('BASE','d9p5jd8s400ip5');
 
 #Caminhos absolutos
 // $dirInt="Projeto_Web_I_Oficial/IFHelth-Beta/";
