2faac0a0aec31537a4b3954cdbba1a969462362c
Ícaro Dias dos Santos <icarodias2222@gmail.com>
2022-07-25 09:45:39 -0300

    Update variaveis.php

--- model/DB/variaveis.php
+++ model/DB/variaveis.php
@@ -8,49 +8,4 @@
 // define('PASS', '');
 // define('BASE', 'ifhealth');
 
-
-// ------------ Banco Para Hospedar no Mysql --------------
-
-// define('USER','id17504595_if');
-// define('PASS','YhR-nOB8l&K+/M9Y');
-// define('BASE','id17504595_ifhealth');
-
-// ------------ Banco Para Hospedar no Pgsql Ceteia --------------
-
-// define('HOST','200.128.100.19');
-// define('USER','aluno');
-// define('PASS','aluno2021');
-// define('PORT','15432');
-// define('BASE','tabd');
-
-// ------------ Banco Para Hospedar no Pgsql SisFis --------------
-
-// define('HOST','200.128.100.19');
-// define('USER','sisfis');
-// define('PASS','sisfis2022');
-// define('PORT','15432');
-// define('BASE','sisfis');
-
-
-// define('HOST','postgres');
-// define('USER','sisfis');
-// define('PASS','sisfis2022');
-// define('PORT','5432');
-// define('BASE','sisfis');
-
-// ------------ Banco Para Hospedar no Pgsql --------------
-
-define('HOST','ec2-52-72-56-59.compute-1.amazonaws.com');
-define('USER','zhxqqnaojtwbpu');
-define('PASS','5147bd0fc1127ed10f697b76e341d823c15cc4814db88c2c3b6d601218f7f8e3');
-define('PORT','5432');
-define('BASE','d9p5jd8s400ip5');
-
-#Caminhos absolutos
-// $dirInt="Projeto_Web_I_Oficial/IFHelth-Beta/";
-// define('DIRPAGE',"http://{$_SERVER['HTTP_HOST']}/{$dirInt}");
-
-// $bar=(substr($_SERVER['DOCUMENT_ROOT'],-1)=='/Projeto_Web_I_Oficial/IFHelth-Beta/')?"":"/Projeto_Web_I_Oficial/IFHelth-Beta/";
-// define('DIRREQ',"{$_SERVER['DOCUMENT_ROOT']}{$bar}{$dirInt}");
-
 ?>
